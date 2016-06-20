289148dd05b91cd177a56b876f52171b0a382fe4
Tony Narlock <tony@git-pull.com>
2016-06-20 10:50:36 -0500

    Update shortcut api documentation

--- libvcs/shortcuts.py
+++ libvcs/shortcuts.py
@@ -6,12 +6,33 @@ from libvcs.exc import InvalidPipURL, InvalidVCS
 
 
 def create_repo(url, vcs, **kwargs):
-    r"""Return object with base class :class:`BaseRepo` depending on url.
+    r"""Return a object representation of a VCS repository.
 
-    Return instance of :class:`libvcs.svn.SubversionRepo`,
-    :class:`libvcs.git.GitRepo` or
-    :class:`libvcs.hg.MercurialRepo`.
-    The object returned is a child of :class:`libvcs.base.BaseRepo`."""
+    :returns: instance of a repository object
+    :rtype: :class:`libvcs.svn.SubversionRepo`, :class:`libvcs.git.GitRepo` or
+        :class:`libvcs.hg.MercurialRepo`.
+
+    Usage Example::
+
+        In [1]: from libvcs import create_repo
+
+        In [2]: r = create_repo_from_pip_url(
+                    url='https://www.github.com/you/myrepo',
+                    vcs='git',
+                    repo_dir='/tmp/myrepo')
+
+        In [3]: r.update_repo()
+        |myrepo| (git)  Repo directory for myrepo (git) does not exist @ \
+            /tmp/myrepo
+        |myrepo| (git)  Cloning.
+        |myrepo| (git)  git clone https://www.github.com/tony/myrepo \
+            /tmp/myrepo
+        Cloning into '/tmp/myrepo'...
+        Checking connectivity... done.
+        |myrepo| (git)  git fetch
+        |myrepo| (git)  git pull
+        Already up-to-date.
+    """
     if vcs == 'git':
         return GitRepo(url, **kwargs)
     elif vcs == 'hg':
@@ -23,27 +44,25 @@ def create_repo(url, vcs, **kwargs):
 
 
 def create_repo_from_pip_url(pip_url, **kwargs):
-    r"""Return object with base class :class:`BaseRepo` depending on url.
+    r"""Return a object representation of a VCS repository via pip-style url.
 
-    Return instance of :class:`libvcs.svn.SubversionRepo`,
-    :class:`libvcs.git.GitRepo` or
-    :class:`libvcs.hg.MercurialRepo`.
-    The object returned is a child of :class:`libvcs.base.BaseRepo`.
+    :returns: instance of a repository object
+    :rtype: :class:`libvcs.svn.SubversionRepo`, :class:`libvcs.git.GitRepo` or
+        :class:`libvcs.hg.MercurialRepo`.
 
     Usage Example::
 
         In [1]: from libvcs import create_repo_from_pip_url
 
         In [2]: r = create_repo_from_pip_url(
-                    url='git+https://www.github.com/you/myrepo',
-                    parent_dir='/tmp/',
-                    name='myrepo')
+                    pip_url='git+https://www.github.com/you/myrepo',
+                    repo_dir='/tmp/myrepo')
 
         In [3]: r.update_repo()
         |myrepo| (git)  Repo directory for myrepo (git) does not exist @ \
             /tmp/myrepo
         |myrepo| (git)  Cloning.
-        |myrepo| (git)  git clone --progress https://www.github.com/tony/myrepo
+        |myrepo| (git)  git clone https://www.github.com/tony/myrepo \
             /tmp/myrepo
         Cloning into '/tmp/myrepo'...
         Checking connectivity... done.
