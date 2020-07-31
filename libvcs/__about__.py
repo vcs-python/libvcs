f543d54ae8950a3c38f0b0f525b1ee932ca53ecf
Tony Narlock <tony@git-pull.com>
2020-07-31 15:46:59 -0500

    Tag v0.4.0rc1

--- libvcs/__about__.py
+++ libvcs/__about__.py
@@ -1,7 +1,7 @@
 __title__ = 'libvcs'
 __package_name__ = 'libvcs'
 __description__ = 'vcs abstraction layer'
-__version__ = '0.3.3'
+__version__ = '0.4.0rc1'
 __author__ = 'Tony Narlock'
 __github__ = 'https://github.com/vcs-python/libvcs'
 __docs__ = 'https://libvcs.git-pull.com'
