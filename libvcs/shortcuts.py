0d3000196846ba5e67d0824f6d487a28b3d8bc8e
Tony Narlock <tony@git-pull.com>
2020-08-02 17:39:08 -0500

    docs(hg,svn,util,shortcuts): Update to use markdown, numpy styling

--- libvcs/shortcuts.py
+++ libvcs/shortcuts.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Shortcuts"""
 from __future__ import absolute_import, print_function, unicode_literals
 
 from libvcs import GitRepo, MercurialRepo, SubversionRepo
@@ -8,20 +9,23 @@ from libvcs.exc import InvalidPipURL, InvalidVCS
 def create_repo(url, vcs, **kwargs):
     r"""Return a object representation of a VCS repository.
 
-    :returns: instance of a repository object
-    :rtype: :class:`libvcs.svn.SubversionRepo`, :class:`libvcs.git.GitRepo` or
-        :class:`libvcs.hg.MercurialRepo`.
+    Returns
+    -------
+    `libvcs.svn.SubversionRepo`, `libvcs.git.GitRepo`, or `libvcs.hg.MercurialRepo`.
 
-    Usage Example::
+    Examples
+    --------
 
-        >>> from libvcs.shortcuts import create_repo
+    >>> from libvcs.shortcuts import create_repo
+    >>>
+    >>> r = create_repo(
+    ...     url='https://www.github.com/you/myrepo',
+    ...     vcs='git',
+    ...     repo_dir='/tmp/myrepo'
+    ... )
 
-        >>> r = create_repo(
-        ...         url='https://www.github.com/you/myrepo',
-        ...         vcs='git',
-        ...         repo_dir='/tmp/myrepo')
+    >>> r.update_repo()
 
-        >>> r.update_repo()
         |myrepo| (git)  Repo directory for myrepo (git) does not exist @ \
             /tmp/myrepo
         |myrepo| (git)  Cloning.
@@ -46,29 +50,30 @@ def create_repo(url, vcs, **kwargs):
 def create_repo_from_pip_url(pip_url, **kwargs):
     r"""Return a object representation of a VCS repository via pip-style url.
 
-    :returns: instance of a repository object
-    :rtype: :class:`libvcs.svn.SubversionRepo`, :class:`libvcs.git.GitRepo` or
-        :class:`libvcs.hg.MercurialRepo`.
+    Returns
+    -------
+    `libvcs.svn.SubversionRepo`, `libvcs.git.GitRepo`, or `libvcs.hg.MercurialRepo`.
 
-    Usage Example::
+    Examples
+    --------
 
-        >>> from libvcs.shortcuts import create_repo_from_pip_url
+    >>> from libvcs.shortcuts import create_repo_from_pip_url
 
-        >>> r = create_repo_from_pip_url(
-        ...         pip_url='git+https://www.github.com/you/myrepo',
-        ...         repo_dir='/tmp/myrepo')
+    >>> r = create_repo_from_pip_url(
+    ...         pip_url='git+https://www.github.com/you/myrepo',
+    ...         repo_dir='/tmp/myrepo')
 
-        >>> r.update_repo()
-        |myrepo| (git)  Repo directory for myrepo (git) does not exist @ \
-            /tmp/myrepo
-        |myrepo| (git)  Cloning.
-        |myrepo| (git)  git clone https://www.github.com/tony/myrepo \
-            /tmp/myrepo
-        Cloning into '/tmp/myrepo'...
-        Checking connectivity... done.
-        |myrepo| (git)  git fetch
-        |myrepo| (git)  git pull
-        Already up-to-date.
+    >>> r.update_repo()
+    |myrepo| (git)  Repo directory for myrepo (git) does not exist @ \
+        /tmp/myrepo
+    |myrepo| (git)  Cloning.
+    |myrepo| (git)  git clone https://www.github.com/tony/myrepo \
+        /tmp/myrepo
+    Cloning into '/tmp/myrepo'...
+    Checking connectivity... done.
+    |myrepo| (git)  git fetch
+    |myrepo| (git)  git pull
+    Already up-to-date.
     """
     if pip_url.startswith('git+'):
         return GitRepo.from_pip_url(pip_url, **kwargs)
