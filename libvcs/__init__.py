b12060e0309f690ad53b2407d0a72c05e7106fdf
Tony Narlock <tony@git-pull.com>
2020-08-02 18:19:23 -0500

    docs: Remove tilde style header

--- libvcs/__init__.py
+++ libvcs/__init__.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Repo package for libvcs.
-
-libvcs
-~~~~~~
-
-"""
+"""Repo package for libvcs."""
 from __future__ import absolute_import, print_function, unicode_literals
 
 import logging
