683770948e14ba814b48f0072996d71f7adac448
Tony Narlock <tony@git-pull.com>
2021-06-16 14:03:20 -0500

    Tag v0.10.0: Convert to markdown

--- libvcs/__about__.py
+++ libvcs/__about__.py
@@ -1,7 +1,7 @@
 __title__ = 'libvcs'
 __package_name__ = 'libvcs'
 __description__ = 'vcs abstraction layer'
-__version__ = '0.9.0'
+__version__ = '0.10.0'
 __author__ = 'Tony Narlock'
 __github__ = 'https://github.com/vcs-python/libvcs'
 __docs__ = 'https://libvcs.git-pull.com'
