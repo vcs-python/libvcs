2b5a63ef6ca2b8bf9107b3e87074578f850295a4
Tony Narlock <tony@git-pull.com>
2022-03-12 07:51:52 -0600

    test(hg): Add default config

--- tests/test_hg.py
+++ tests/test_hg.py
@@ -1,6 +1,8 @@
 """Tests for libvcs hg repos."""
+import getpass
 import os
 import pathlib
+import textwrap
 
 import pytest
 
@@ -9,6 +11,52 @@ from libvcs.util import run, which
 
 if not which("hg"):
     pytestmark = pytest.mark.skip(reason="hg is not available")
+
+
+@pytest.fixture(autouse=True, scope="session")
+def home_path(tmp_path_factory: pytest.TempPathFactory):
+    return tmp_path_factory.mktemp("home")
+
+
+@pytest.fixture(autouse=True, scope="session")
+def user_path(home_path: pathlib.Path):
+    p = home_path / getpass.getuser()
+    p.mkdir()
+    return p
+
+
+@pytest.fixture(autouse=True, scope="session")
+def hg_user_path(user_path: pathlib.Path):
+    hg_config = user_path / ".hg"
+    hg_config.mkdir()
+    return hg_config
+
+
+@pytest.fixture(autouse=True, scope="session")
+def hgrc(hg_user_path: pathlib.Path):
+    hgrc = hg_user_path / "hgrc"
+    hgrc.write_text(
+        textwrap.dedent(
+            f"""
+        [paths]
+        default = {hg_remote}
+
+        [ui]
+        username = libvcs tests <libvcs@git-pull.com>
+        merge = internal:merge
+
+        [trusted]
+        users = {getpass.getuser()}
+    """
+        ),
+        encoding="utf-8",
+    )
+    return hgrc
+
+
+@pytest.fixture(autouse=True)
+def hgrc_default(monkeypatch: pytest.MonkeyPatch, hgrc: pathlib.Path):
+    monkeypatch.chdir(hgrc.parent)
 
 
 @pytest.fixture
