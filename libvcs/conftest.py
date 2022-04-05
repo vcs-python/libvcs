d449f3bade8d94b0edc6ff7cd2e58bec7ef5fa2a
Tony Narlock <tony@git-pull.com>
2022-04-05 07:56:56 -0500

    test: Stub libvcs.conftest.py (move tests/conftest.py here later?)

---
+++ libvcs/conftest.py
@@ -0,0 +1,4 @@
+# TODO: Move tests/conftest.py here if we go along with doctest?
+# - https://github.com/numpy/numpy/blob/v1.22.3/numpy/conftest.py
+# - https://docs.pytest.org/en/6.2.x/doctest.html
+from tests.conftest import *  # noqa F40
