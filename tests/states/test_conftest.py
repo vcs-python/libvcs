900c1c9531d464f8f81e1b469aca9f204559c958
Tony Narlock <tony@git-pull.com>
2022-04-10 10:07:51 -0500

    tests(test_conftest): Test both remote repo fixture factories

---
+++ tests/states/test_conftest.py
@@ -0,0 +1,25 @@
+import pathlib
+
+from libvcs.conftest import CreateRepoCallbackFixProtocol
+
+
+def test_create_git_remote_repo(
+    create_git_remote_repo: CreateRepoCallbackFixProtocol,
+    tmp_path: pathlib.Path,
+    projects_path: pathlib.Path,
+):
+    git_remote_1 = create_git_remote_repo()
+    git_remote_2 = create_git_remote_repo()
+
+    assert git_remote_1 != git_remote_2
+
+
+def test_create_svn_remote_repo(
+    create_svn_remote_repo: CreateRepoCallbackFixProtocol,
+    tmp_path: pathlib.Path,
+    projects_path: pathlib.Path,
+):
+    svn_remote_1 = create_svn_remote_repo()
+    svn_remote_2 = create_svn_remote_repo()
+
+    assert svn_remote_1 != svn_remote_2
