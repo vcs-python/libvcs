d84dcba1ca0269af429ab8654eaca138152a184f
Tony Narlock <tony@git-pull.com>
2022-05-14 03:58:02 -0500

    chore(types): Add TypeAlias

--- libvcs/types.py
+++ libvcs/types.py
@@ -1,9 +1,9 @@
 from os import PathLike
-from typing import Union
+from typing import TypeAlias, Union
 
 # via github.com/python/typeshed/blob/5df8de7/stdlib/_typeshed/__init__.pyi#L115-L118
 
 #: :class:`os.PathLike` or :func:`str`
-StrPath = Union[str, PathLike[str]]  # stable
+StrPath: TypeAlias = Union[str, PathLike[str]]  # stable
 #: :class:`os.PathLike`, :func:`str` or :term:`bytes-like object`
-StrOrBytesPath = Union[str, bytes, PathLike[str], PathLike[bytes]]  # stable
+StrOrBytesPath: TypeAlias = Union[str, bytes, PathLike[str], PathLike[bytes]]  # stable
