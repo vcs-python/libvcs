7c2dd7cec8fc77e83caa16d18bd2fc72f1da8ad2
Tony Narlock <tony@git-pull.com>
2022-05-14 04:10:37 -0500

    docs(types): Document libvcs.types

--- libvcs/types.py
+++ libvcs/types.py
@@ -1,9 +1,17 @@
+"""Internal :term:`type annotations <annotation>`
+
+Notes
+-----
+
+:class:`StrPath` and :class:`StrOrBytesPath` is based on `typeshed's`_.
+
+.. _typeshed's: https://github.com/python/typeshed/blob/5df8de7/stdlib/_typeshed/__init__.pyi#L115-L118
+"""  # NOQA E501
 from os import PathLike
 from typing import TypeAlias, Union
 
-# via github.com/python/typeshed/blob/5df8de7/stdlib/_typeshed/__init__.pyi#L115-L118
-
-#: :class:`os.PathLike` or :func:`str`
 StrPath: TypeAlias = Union[str, PathLike[str]]  # stable
-#: :class:`os.PathLike`, :func:`str` or :term:`bytes-like object`
+""":class:`os.PathLike` or :class:`str`"""
+
 StrOrBytesPath: TypeAlias = Union[str, bytes, PathLike[str], PathLike[bytes]]  # stable
+""":class:`os.PathLike`, :class:`str` or :term:`bytes-like object`"""
