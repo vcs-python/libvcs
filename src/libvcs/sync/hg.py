76e1d69a0b9c79f58dee38ca596b0e69a924d715
Tony Narlock <tony@git-pull.com>
2022-10-13 18:49:59 -0500

    refactor(sync[hg]): Move to cmd

--- src/libvcs/sync/hg.py
+++ src/libvcs/sync/hg.py
@@ -12,6 +12,8 @@ import logging
 import pathlib
 from typing import Any
 
+from libvcs.cmd.hg import Hg
+
 from .base import BaseSync
 
 logger = logging.getLogger(__name__)
@@ -22,21 +24,26 @@ class HgSync(BaseSync):
     schemes = ("hg", "hg+http", "hg+https", "hg+file")
 
     def obtain(self, *args: Any, **kwargs: Any) -> None:
-        self.ensure_dir()
-
-        # Double hyphens between [OPTION]... -- SOURCE [DEST] prevent command injections
-        # via aliases
-        self.run(["clone", "--noupdate", "-q", "--", self.url, str(self.dir)])
-        self.run(["update", "-q"])
+        cmd = Hg(dir=self.dir)
+        cmd.clone(
+            no_update=True,
+            quiet=True,
+            url=self.url,
+        )
+        cmd.update(
+            quiet=True,
+            check_returncode=True,
+        )
 
     def get_revision(self) -> str:
         return self.run(["parents", "--template={rev}"])
 
     def update_repo(self, *args: Any, **kwargs: Any) -> None:
-        self.ensure_dir()
+        cmd = Hg(dir=self.dir)
+
         if not pathlib.Path(self.dir / ".hg").exists():
             self.obtain()
             self.update_repo()
         else:
-            self.run(["update"])
-            self.run(["pull", "-u"])
+            cmd.update()
+            cmd.pull(update=True)
