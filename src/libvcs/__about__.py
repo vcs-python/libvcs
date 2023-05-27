c3683e60eb9fc67cee3463a67f7545e1abe4d576
Tony Narlock <tony@git-pull.com>
2023-05-27 12:03:32 -0500

    Tag v0.22.0 (addition of ruff)

--- src/libvcs/__about__.py
+++ src/libvcs/__about__.py
@@ -1,7 +1,7 @@
 __title__ = "libvcs"
 __package_name__ = "libvcs"
 __description__ = "Lite, typed, python utilities for Git, SVN, Mercurial, etc."
-__version__ = "0.21.2"
+__version__ = "0.22.0"
 __author__ = "Tony Narlock"
 __github__ = "https://github.com/vcs-python/libvcs"
 __docs__ = "https://libvcs.git-pull.com"
