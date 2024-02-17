bdc4e6fb027bc52a52dff753bdb8faaa94beba6b
Tony Narlock <tony@git-pull.com>
2024-02-17 05:21:01 -0600

    Tag v0.28.2 (bug fix for Git.rev_list, CI bump)

--- src/libvcs/__about__.py
+++ src/libvcs/__about__.py
@@ -2,7 +2,7 @@
 __title__ = "libvcs"
 __package_name__ = "libvcs"
 __description__ = "Lite, typed, python utilities for Git, SVN, Mercurial, etc."
-__version__ = "0.28.1"
+__version__ = "0.28.2"
 __author__ = "Tony Narlock"
 __github__ = "https://github.com/vcs-python/libvcs"
 __docs__ = "https://libvcs.git-pull.com"
