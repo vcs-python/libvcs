a0ba4bc1200de2b35c4f838c085049f92a93be57
Tony Narlock <tony@git-pull.com>
2024-10-13 06:43:44 -0500

    refactor(test[pytest plugin]) Consolidate `pytester` test

--- tests/test_pytest_plugin.py
+++ tests/test_pytest_plugin.py
@@ -36,83 +36,7 @@ def test_create_svn_remote_repo(
     assert svn_remote_1 != svn_remote_2
 
 
-def test_create_git_remote_repo_and_git_sync(
-    pytester: pytest.Pytester,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    """Tests pytest plugin integration for create_git_remote_repo() and GitSync()."""
-    # Initialize variables
-    pytester.plugins = ["pytest_plugin"]
-    pytester.makefile(
-        ".ini",
-        pytest=textwrap.dedent(
-            """
-[pytest]
-addopts=-vv
-        """.strip(),
-        ),
-    )
-    pytester.makeconftest(
-        textwrap.dedent(
-            r"""
-import pathlib
-import pytest
-
-@pytest.fixture(autouse=True)
-def setup(
-    request: pytest.FixtureRequest,
-    gitconfig: pathlib.Path,
-    set_home: pathlib.Path,
-) -> None:
-    pass
-    """,
-        ),
-    )
-    tests_path = pytester.path / "tests"
-    files = {
-        "example.py": textwrap.dedent(
-            """
-import pathlib
-
-from libvcs.sync.git import GitSync
-from libvcs.pytest_plugin import CreateRepoPytestFixtureFn
-
-def test_repo_git_remote_repo_and_sync(
-    create_git_remote_repo: CreateRepoPytestFixtureFn,
-    tmp_path: pathlib.Path,
-    projects_path: pathlib.Path,
-) -> None:
-    git_server = create_git_remote_repo()
-    git_repo_checkout_dir = projects_path / "my_git_checkout"
-    git_repo = GitSync(path=str(git_repo_checkout_dir), url=f"file://{git_server!s}")
-
-    git_repo.obtain()
-    git_repo.update_repo()
-
-    assert git_repo.get_revision() == "initial"
-
-    assert git_repo_checkout_dir.exists()
-    assert pathlib.Path(git_repo_checkout_dir / ".git").exists()
-        """,
-        ),
-    }
-    first_test_key = next(iter(files.keys()))
-    first_test_filename = str(tests_path / first_test_key)
-
-    tests_path.mkdir()
-    for file_name, text in files.items():
-        test_file = tests_path / file_name
-        test_file.write_text(
-            text,
-            encoding="utf-8",
-        )
-
-    # Test
-    result = pytester.runpytest(str(first_test_filename))
-    result.assert_outcomes(passed=1)
-
-
-def test_git_remote_repo(
+def test_git_fixtures(
     pytester: pytest.Pytester,
     monkeypatch: pytest.MonkeyPatch,
     tmp_path: pathlib.Path,
@@ -163,6 +87,25 @@ from libvcs.pytest_plugin import (
     git_remote_repo_single_commit_post_init
 )
 
+
+def test_repo_git_remote_repo_and_sync(
+    create_git_remote_repo: CreateRepoPytestFixtureFn,
+    tmp_path: pathlib.Path,
+    projects_path: pathlib.Path,
+) -> None:
+    git_server = create_git_remote_repo()
+    git_repo_checkout_dir = projects_path / "my_git_checkout"
+    git_repo = GitSync(path=str(git_repo_checkout_dir), url=f"file://{git_server!s}")
+
+    git_repo.obtain()
+    git_repo.update_repo()
+
+    assert git_repo.get_revision() == "initial"
+
+    assert git_repo_checkout_dir.exists()
+    assert pathlib.Path(git_repo_checkout_dir / ".git").exists()
+
+
 def test_git_bare_repo_sync_and_commit(
     create_git_remote_bare_repo: CreateRepoPytestFixtureFn,
     projects_path: pathlib.Path,
@@ -206,7 +149,7 @@ def test_git_bare_repo_sync_and_commit(
 
     # Test
     result = pytester.runpytest(str(first_test_filename))
-    result.assert_outcomes(passed=1)
+    result.assert_outcomes(passed=2)
 
 
 def test_gitconfig(
