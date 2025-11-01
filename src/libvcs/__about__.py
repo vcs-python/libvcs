7f2c01d83a3e047cd9aeb6d944e4759650242424
Tony Narlock <tony@git-pull.com>
2025-11-01 12:07:15 -0500

    Tag v0.37.0 (Drop Python 3.9)

--- src/libvcs/__about__.py
+++ src/libvcs/__about__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 __title__ = "libvcs"
 __package_name__ = "libvcs"
 __description__ = "Lite, typed, python utilities for Git, SVN, Mercurial, etc."
-__version__ = "0.36.0"
+__version__ = "0.37.0"
 __author__ = "Tony Narlock"
 __github__ = "https://github.com/vcs-python/libvcs"
 __docs__ = "https://libvcs.git-pull.com"
